9812ce397c4ecd3d3b433d401a157b6f168bea84
news.update <59946172+heker-nob@users.noreply.github.com>
2020-04-11 20:24:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<html>
+<title>?</title>
+<script>alert('Indonesian People')</script>
+<meta name='keywords' content='HPH, OTAKU404, ./OT4KU404, Hacked by Hacker Patah Hati, 0wn3d By Hacker Patah Hati , Indonesian Hacker Rulez, Hacked By ./OT4KU404'>
+<meta property="og:image"content="http://pa-waingapu.go.id/images/shouko-nishimiya.png">
+<meta content='Ini Adalah Tekad ku' name='description'>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/222660642&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<style type:="text/css">
+
+    body {
+    
+    background:  #000000;
+    
+    font-family: Hiruko Pro;
+    
+    color: rgb(255, 255, 255);
+    
+    text-align: center;
+    
+    }
+    
+    a {
+    
+    text-decoration:none;
+    
+    color:
+    
+    #fffbfb;
+    
+    }
+    
+    </style>
+    <center>    <img src="http://pa-waingapu.go.id/images/shouko-nishimiya.png" width=887px" height="481px"></center>
+        <font size="6" color="red">---</font><font color="red" size="6">0wn3d by ./OT4KU404</font><font size="6" color="red">---</font>
+       <br></center> <font size="5" color="white">Greetz:</font><font color="white">| M05L3M  | Pandora | ClownTerror072 | Zeylan7 | Z3X | d4ff4 | Rm19 | NCH Tersakiti | </font><br>
+       <font color="red" size="4">Indonesian Hacker Rulez &copy 2018</font>
+                                                                                                                                                                                    </html>
